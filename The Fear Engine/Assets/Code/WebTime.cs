236d45e61b921c8a188a4b8529f5beba125abfde
Eric Williams <ewillms2@uic.edu>
2016-03-17 14:35:22 -0500

    web fix

--- The Fear Engine/Assets/Code/WebTime.cs
+++ The Fear Engine/Assets/Code/WebTime.cs
@@ -10,17 +10,9 @@ public class WebTime : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-<<<<<<< HEAD
-        
-=======
+
 	}
-	void OnTriggerEnter(Collider other) 
-	{
-		if (other.tag != "Spider"){
-			Object.Destroy(this.gameObject)	
-		}
->>>>>>> origin/master
-	}
+
     void OnTriggerEnter(Collider other)
     {
         if (other.tag != "Spider")
