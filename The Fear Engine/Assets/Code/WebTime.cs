a6bdc77294e5623ee8ea30f4ab1700bc403150ef
Eric Williams <ewillms2@uic.edu>
2016-03-17 11:51:46 -0500

    Spidey changed
    Spider has an aggro range now, can be easily adjusted in script.
    Will only shoot the webs if the player is in the sight of the spider
    (using raycast).

---
+++ The Fear Engine/Assets/Code/WebTime.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WebTime : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Object.Destroy(this.gameObject, 2.0f);
+	}
+}
--- The Fear Engine/Assets/Code/enemyAI1.cs
+++ The Fear Engine/Assets/Code/enemyAI1.cs
@@ -11,6 +11,7 @@ public class enemyAI1 : MonoBehaviour {
     public Transform myEnemy;
     public float bulletImpulse = 20.0f;
     public GameObject terrain;
+    private RaycastHit hit;
     //public Transform terrain;
     public int sValue;
     private int moveSpeed = 3;
@@ -19,9 +20,9 @@ public class enemyAI1 : MonoBehaviour {
     private float aggroRange = 30;
     private float waitingTime = 5;
     private float timer;
-
+    private bool seePlayer;
     public bool autoShoot; // to turn off auto shooting behavior in Update() when Shoot() is used in FighterAI
-
+    
     void Awake()
     {
         myEnemy = transform;
@@ -34,12 +35,30 @@ public class enemyAI1 : MonoBehaviour {
 
     void Update()
     {
+        
         var lookDir = targetPlayer.position - transform.position;
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, lookDir, out hit, 100)) // Now the spider only shoots if he sees the player.
+        {
+            if (hit.collider.tag == "Player")
+            {
+                seePlayer = true;
+            }
+            else if (hit.collider.tag == "Wall") // if the spiders see a wall they will not shoot.
+            {
+                seePlayer = false;
+            }
+            
+        }
+        float seperation = Vector3.Distance(this.transform.position, targetPlayer.transform.position);
         lookDir.y = 0;
         // zero the height difference
         myEnemy.rotation = Quaternion.Slerp(myEnemy.rotation, Quaternion.LookRotation(lookDir), rotationSpeed * Time.deltaTime);
-        myEnemy.position += myEnemy.forward * moveSpeed * Time.deltaTime;
-        float seperation = Vector3.Distance(this.transform.position, targetPlayer.transform.position);
+        if (seperation <= aggroRange) //distance the player must be to make the spider move towards him.
+        {
+            myEnemy.position += myEnemy.forward * moveSpeed * Time.deltaTime;
+
+        }
         timer += Time.deltaTime;
         if (seperation <= aggroRange && timer > waitingTime && autoShoot)
         {
@@ -48,7 +67,7 @@ public class enemyAI1 : MonoBehaviour {
             timer = 0;
         }
     }
-
+        
     public void Shoot()
     {
 
