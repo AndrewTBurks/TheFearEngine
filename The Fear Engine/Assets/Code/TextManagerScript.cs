4c74554f39dfd3709025cfc6a13992412538b2be
AndrewTBurks <andrewtburks@gmail.com>
2016-03-30 22:39:29 -0500

    Story Text Management System completed
    SanityManager - Updated the screen images to include Text for the
    messages, and RawImage for the text background.
    
    TextManager - Script takes the Text and RawImage from SanityManager and
    will do the updating of the text on the screen, and the coroutine to
    keep on the screen for a certain amount of time.
    
    TextZone - Includes collider for where the text will be activated if the
    player walks there. Script takes a string for the message that will be
    displayed in this zone. When the player enters the collider, only for
    the first time, the method in TextManager will be called, displaying the
    text to the player for 10 seconds, then disappearing.

---
+++ The Fear Engine/Assets/Code/TextManagerScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TextManagerScript : MonoBehaviour {
+    public Text dialogueText;
+    public RawImage textBG;
+    
+	void Awake () {
+        dialogueText.text = null;
+        textBG.color = new Color(textBG.color.r, textBG.color.g, textBG.color.b, 0);
+    }
+	
+
+    public void DisplayText(string message)
+    {
+        dialogueText.text = message;
+        textBG.color = new Color(textBG.color.r, textBG.color.g, textBG.color.b, 0.2f);
+        StartCoroutine(TextWait(10));
+        
+    }
+
+    IEnumerator TextWait(int time)
+    {
+        yield return new WaitForSeconds(time);
+        dialogueText.text = null;
+        textBG.color = new Color(textBG.color.r, textBG.color.g, textBG.color.b, 0);
+    }
+}
---
+++ The Fear Engine/Assets/Code/TextZoneScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextZoneScript : MonoBehaviour {
+    public string zoneText = "Test";
+
+    bool seen;      // whether the player has already seen the text at this point
+
+    TextManagerScript managerScript;
+
+    void Awake()
+    {
+        seen = false;
+        managerScript = transform.parent.gameObject.GetComponent<TextManagerScript>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" && !seen)
+        {
+            print("Player Entered Zone");
+            managerScript.DisplayText(zoneText);
+            seen = true;
+        }
+        
+    }
+
+}
