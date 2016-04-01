13ffae9db70243bfeb13454eba19f8612e975eca
bravisc <bravisc@outlook.com>
2016-04-01 03:35:52 -0500

    Opening scene and organization
    removed "testfor4-1-2016.exe" as I believe it wasn't allowing me to sync
    
    Starting menu and hierarchy clean up
    
    Created an "opening scene" that
    gives the pretense for the game and the
    setting.  This scene will then
    take us to the scene that we're used to.
    
    *Note: while in the editor,
    when transitioning from the opening scene to
    scene "hmwk8-1" the
    lighting will appear to be messed up.  While I
    haven't figured out how
    to fix the transition in the editor, this isn't
    too much of a big deal
    as this bug doesn't occur once the game is built.
    
    I also cleaned up the
    hierarchy in hmwk8-1 using empty gameobjects as
    folders.  Now its much
    easier to find and organize things.  As long as
    nothing is done to these
    folder objects, everything should run and
    operate as normal.

---
+++ The Fear Engine/Assets/Code/LoadOnClick.cs
@@ -0,0 +1,18 @@
+﻿/*
+Code taken form Unity tutorial
+https://unity3d.com/learn/tutorials/modules/beginner/live-training-archive/creating-a-scene-menu
+*/
+
+using UnityEngine;
+using System.Collections;
+
+public class LoadOnClick : MonoBehaviour {
+
+    //public GameObject loadingImage;
+
+	public void LoadScene(int level)
+    {
+        //loadingImage.SetActive(true);
+        Application.LoadLevel(level);
+    }
+}
---
+++ The Fear Engine/Assets/Code/Menu.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour {
+
+
+    public Canvas Quitmenu;
+    public Button startText;
+    //public Button exitText;
+
+
+	// Use this for initialization
+	void Start () {
+
+
+	//Quitmenu = Quitmenu.GetComponent<Canvas>();
+        startText = startText.GetComponent<Button>();
+        //exitText = exitText.GetComponent<Button>();
+      //  Quitmenu.enabled = false;
+	}
+	
+	public void ExitPress()
+    {
+        //Quitmenu.enabled = true;
+        startText.enabled= false;
+        //exitText.enabled=false;
+        //Debug.Log("ExitPressed");
+    }
+
+	public void NoPress()
+    {
+        //Quitmenu.enabled = false;
+        startText.enabled = true;
+        //exitText.enabled = true;
+    }
+	
+    public void StartLevel()
+    {
+        Application.LoadLevel("hmwk8-2");
+        Debug.Log("Start game pressed");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
