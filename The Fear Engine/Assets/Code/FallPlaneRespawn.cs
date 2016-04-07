21c9d1fd5af5cb6cdea165314358abc140901713
bravisc <bravisc@outlook.com>
2016-04-06 22:49:56 -0500

    Falling dead zones and respawning
    Created a basic script that will respawn the player if they fall off the
    level.  The player will then respawn at start with half of their current
    sanity.
    
    It's fairly easy to set up, just create a plane that covers the level's
    fall zones and place an empty child object where you would like the
    player to respawn.  The script will compare the y coordinates of both
    the fallplane and the player and will the respawn the player if they
    fall below it - more info is commented in the script.

---
+++ The Fear Engine/Assets/Code/FallPlaneRespawn.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/* 
+  This script checks the position of the player character, and if it ever falls below the position of the "fallPlane", the player is set to respawn at
+  the "spawnlocation" with their sanity decremented (I'm thinking maybe - 50% of current sanity).  Note that this script is just for
+  level "killzone", it probably wouldn't work well for random pits
+
+    playerChar should be the FPScontroller
+    spawnlocation should be an empty child object
+    fallplane is just plane
+*/
+public class FallPlaneRespawn : MonoBehaviour {
+
+    public Transform fallPlane;
+    public Transform playerChar;
+    public SanityManagerScript manager;
+    public Transform spawnlocation;
+    private float san;
+    
+
+	// Use this for initialization
+	void Start () {
+
+      
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+     
+           if ((playerChar.position.y <= fallPlane.position.y))
+        {
+            playerChar.position = spawnlocation.position;
+            playerChar.rotation = spawnlocation.rotation;
+            san = manager.GetSanity();
+            san = san / 2;
+            manager.AddSanity(-san);
+
+        }
+	}
+}
