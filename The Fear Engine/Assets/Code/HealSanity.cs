244388dc3112108566547fdda8da11cc29a0e1e5
bravisc <bravisc@outlook.com>
2016-04-08 03:34:44 -0500

    Added Health Pick Up, touched up Alpha build
    Added green health pick ups - they restore up to 50% of the players
    sanity.  I've placed two in out of the way areas in the 1st level
    
    Added a few enemies as a penalty for failing the 2nd platforming puzzle.
    
    Jumping level now has music as well.

---
+++ The Fear Engine/Assets/Code/HealSanity.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealSanity : MonoBehaviour {
+
+    public GameObject healObject;
+    public Transform targetPlayer;
+    public SanityManagerScript sanity;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float seperation = Vector3.Distance(this.transform.position, targetPlayer.transform.position);
+
+        if ((seperation <= 2))
+        {
+            float tSan;
+            tSan = sanity.GetSanity();
+            if (tSan >= 50)
+            {
+                sanity.UpdateSanity(100);
+            }
+            else
+            {
+                sanity.UpdateSanity(tSan + 50);
+            }
+
+            Destroy(healObject);
+        }
+    }
+
+    
+}
