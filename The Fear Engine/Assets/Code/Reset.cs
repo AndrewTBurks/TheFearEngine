38f4bf99354e5be4f30319505e98c3be5caab7ec
Eric Williams <ewillms2@uic.edu>
2016-04-26 16:46:30 -0500

    Jump Level
    press and hold 1,2 now in order to teleport.

--- The Fear Engine/Assets/Code/Reset.cs
+++ The Fear Engine/Assets/Code/Reset.cs
@@ -1,15 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using UnityEngine.UI;
 public class Reset : MonoBehaviour {
     public Vector3 checkPoint;
     public Rigidbody player;
     public SanityManagerScript playerhealth;
-    public GameObject firstTP;
-    public GameObject secondTP;
-    public GameObject thirdTP;
     public Camera cam;
     private float numCheckPoints;
+    private float timer;
+    private float chargeTime;
+    private bool charging;
+    private bool chargingtwo;
+    private float timertwo;
+    public Slider slider;
     // Use this for initialization
     void Start () {
         checkPoint = player.transform.position;
@@ -18,19 +21,67 @@ public class Reset : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        
 	    if (Input.GetKeyDown("1"))
+        {
+            charging = true;
+            
+        }
+        if (Input.GetKeyUp("1"))
+        {
+            charging = false;
+            timer = 0;
+            slider.value = timer;
+        }
+        if (timer >= 1.0f)
         {
             numCheckPoints -= 1;
             checkPoint = this.transform.position;
+            timer = 0;
+            slider.value = timer;
         }
 
         if (Input.GetKeyDown("2"))
         {
-            this.transform.position = checkPoint;
-            playerhealth.AddSanity(-5);
+            chargingtwo = true;
         }
 
+        if (Input.GetKeyUp("2"))
+        {
+            chargingtwo = false;
+            timertwo = 0;
+            slider.value = timertwo;
+        }
 
+        if (timertwo >= 1.0f)
+        {
+            this.transform.position = checkPoint;
+            playerhealth.AddSanity(-5);
+            timertwo = 0;
+            slider.value = timertwo;
+        }
+
+        if (charging)
+        {
+            timer += Time.deltaTime;
+            slider.value = timer;
+        }
+
+        if (chargingtwo)
+        {
+            timertwo += Time.deltaTime;
+            slider.value = timertwo;
+        }
+
+        if (Input.GetKeyDown("q"))
+        {
+            Physics.gravity = new Vector3(1f, 5f, 0f);
+        }
+
+        if (Input.GetKeyUp("q"))
+        {
+            Physics.gravity = new Vector3(1f, -9.81f, 0f);
+        }
     }
 
     void OnTriggerEnter(Collider other)
@@ -38,17 +89,7 @@ public class Reset : MonoBehaviour {
         if (other.tag == "FLOOR")
         {
             this.transform.position = checkPoint;
-        }
-
-        if (other.tag == "BossTP")
-        {
-            this.transform.position = firstTP.transform.position;
-            cam.farClipPlane = 1000;
-
-        }
-        if (other.tag == "BossTP2")
-        {
-            this.transform.position = secondTP.transform.position;
+            playerhealth.AddSanity(-5);
         }
        
     }
---
+++ The Fear Engine/Assets/Code/UImanager.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UImanager : MonoBehaviour {
+
+    public GameObject pausePanel;
+    private GameObject cam;
+    private GameObject player;
+    public bool isPaused;
+	// Use this for initialization
+	void Start () {
+        isPaused = false;
+        cam = GameObject.Find("MainCamera");
+        player = GameObject.Find("Player");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    if (isPaused)
+        {
+            PauseGame(true);
+        }
+        else
+        {
+            PauseGame(false);
+        }
+
+        if (Input.GetButtonDown("Cancel"))
+        {
+            SwitchPause();
+        }
+	}
+
+    void PauseGame(bool state)
+    {
+        if (state)
+        {
+            pausePanel.SetActive(true);
+            Time.timeScale = 0.0f;
+        }
+        else
+        {
+            Time.timeScale = 1.0f;
+            pausePanel.SetActive(false);
+        }
+    }
+
+    public void SwitchPause()
+    {
+        if (isPaused)
+        {
+            isPaused = false; //Changes the value
+        }
+
+        else
+        {
+            isPaused = true;
+        }
+    }
+}
