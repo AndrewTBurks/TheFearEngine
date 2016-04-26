58ccd4c9d6f8fc196cbfc7617b12006731d2534b
Eric Williams <ewillms2@uic.edu>
2016-04-26 17:55:32 -0500

    Added checkpoints and textmanager to jump level

---
+++ The Fear Engine/Assets/stickyScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class stickyScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            transform.parent = other.transform;
+        }
+    }
+}
