238263ba795744164d31a6093a4a5259be64746c
AndrewTBurks <andrewtburks@gmail.com>
2016-04-29 00:21:49 -0500

    SanityHeal Despawn fixed
    Now the SanityHeal does despawn after 8 seconds, and it spawns in a
    range centered around the player, within the boundaries of the corners.

--- The Fear Engine/Assets/Code/SanityHealSpawner.cs
+++ The Fear Engine/Assets/Code/SanityHealSpawner.cs
@@ -10,7 +10,8 @@ public class SanityHealSpawner : MonoBehaviour {
 
     public GameObject sanityHealPrefab;
     public float spawningDelay = 8;
-    public float despawnDelay = 10;
+
+    public float spawnRadius = 30;
 
 	void Awake()
     {
@@ -21,28 +22,54 @@ public class SanityHealSpawner : MonoBehaviour {
     {
         print("Spawning Regen Obj");
 
-        Vector3 range = corner2.transform.position - corner1.transform.position;
-        float xRange = range.x;
-        float zRange = range.z;
+        Vector3 playerPos = player.transform.position;
+
+        float xMin = Mathf.Min(corner1.transform.position.x, corner2.transform.position.x);
+        float xMax = Mathf.Max(corner1.transform.position.x, corner2.transform.position.x);
+        float zMin = Mathf.Min(corner1.transform.position.z, corner2.transform.position.z);
+        float zMax = Mathf.Max(corner1.transform.position.z, corner2.transform.position.z);
+
+        float xRand = Random.Range(playerPos.x - spawnRadius, playerPos.x + spawnRadius);
+        float zRand = Random.Range(playerPos.z - spawnRadius, playerPos.z + spawnRadius);
+
+        float xSpawn;
+        float zSpawn;
+
+        // find corrext X spawn
+        if (xRand > xMax)
+            xSpawn = xMax;
+        else if (xRand < xMin)
+            xSpawn = xMin;
+        else
+            xSpawn = xRand;
+
+        // find correct Z spawn
+        if (zRand > zMax)
+            zSpawn = zMax;
+        else if (zRand < zMin)
+            zSpawn = zMin;
+        else
+            zSpawn = zRand;
+
+
 
         GameObject newObj = Instantiate(sanityHealPrefab, 
-                                        new Vector3(corner1.transform.position.x + Random.Range(0.0f, xRange), 
+                                        new Vector3(xSpawn, 
                                                     transform.position.y, 
-                                                    corner1.transform.position.z + Random.Range(0.0f, zRange)), 
+                                                    zSpawn), 
                                         transform.rotation) as GameObject;
 
-        newObj.AddComponent<EnemyTimeout>();
-        newObj.GetComponent<EnemyTimeout>().waitTime = despawnDelay;
         newObj.GetComponent<HealSanity>().healObject = newObj;
         newObj.GetComponent<HealSanity>().targetPlayer = player;
         newObj.GetComponent<HealSanity>().sanity = sanityManager;
 
-        StartCoroutine(SpawnHealerWait(spawningDelay));
+        StartCoroutine(SpawnHealerWait(spawningDelay, newObj));
     }
 
-    IEnumerator SpawnHealerWait(float waitTime)
+    IEnumerator SpawnHealerWait(float waitTime, GameObject thisHealObj)
     {
         yield return new WaitForSeconds(waitTime);
+        Destroy(thisHealObj);
         SpawnHealer();
     }
 }
