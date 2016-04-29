4877252e6a5b7fd57b2fba5908852f5d4f93c56c
bravisc <bravisc@outlook.com>
2016-04-28 23:17:04 -0500

    Added LoadLevel scene + also edited loss scene

---
+++ The Fear Engine/Assets/Code/SelectLevel.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SelectLevel : MonoBehaviour {
+
+    public Dropdown myDropdown;
+
+
+	// Use this for initialization
+	public void chooseLevel()
+    {
+        myDropdown.onValueChanged.AddListener(delegate{ myDropdownValueChangedHandler(myDropdown); });
+    }
+    private void myDropdownValueChangedHandler(Dropdown target)
+    {
+        Debug.Log("selected: " + target.value);
+    }
+
+    public void SetDropdownIndex(int index)
+    {
+        myDropdown.value = index;
+    }
+}
